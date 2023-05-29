bb36826ae302c6511c11e34196ce8a9e688303b0
Banus <edwin@ebait.nl>
2023-05-29 23:15:19 +0200

    Adding Rumble, 3D viewer and Memoryleak

--- Arduino/Final/Final.ino
+++ Arduino/Final/Final.ino
@@ -1,13 +1,26 @@
 #include <Servo.h>
+#include <Adafruit_INA219.h>
+
+
+
 #define LEFTSERVOPIN 9 //Orange PWM, RED +5V, Brown GND
 #define RIGHTSERVOPIN 10
+#define LINSERVOPIN 11
 
 Servo leftservo;
 Servo rightservo;
 Servo linservo;
 
+Adafruit_INA219 ina219;
+
 byte buf[2];
 int newData;
+
+long current_millis = 0;
+long prev_millis = 0;
+int counter = 0;
+
+
 
 void getData() {
   if (Serial.available() != 0) {
@@ -20,9 +33,26 @@ void setup() {
   Serial.begin(115200);       
   leftservo.attach(LEFTSERVOPIN);
   rightservo.attach(RIGHTSERVOPIN);
+  linservo.attach(LINSERVOPIN);
+  ina219.begin();
+  uint32_t currentFrequency;
+  delay(100);
 }
 
 void loop() {
+  current_millis = millis();
+  if (current_millis != prev_millis) {
+    counter ++;
+    if (counter > 100) counter = 0;{
+      if (Serial.available() != 0) {
+        float current_mA = 0;
+        current_mA = ina219.getCurrent_mA();
+        Serial.println(current_mA);
+        prev_millis = current_millis;
+      }
+    }
+  }
+
    getData();
    if (newData == true) {
       if ((int)buf[0] == 0)
--- EOD_WPF/MainWindow.xaml.cs
+++ EOD_WPF/MainWindow.xaml.cs
@@ -24,7 +24,10 @@ namespace EOD_WPF
         public MainWindow()
         {
             InitializeComponent();
-            controller.Connection.ValueChanged += (s, e) => new LogItem("Connection succesfull!").print(log);
+            controller.Connection.ValueChanged += (s, e) => {
+                new LogItem($"Controller {controller.Connection}").print(log);
+                new LogItem($"Battery {controller.Battery.Value}").print(log);
+                };
 
 
             DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
@@ -50,11 +53,15 @@ namespace EOD_WPF
                 DisplayMemberBinding = new Binding("Message")
             });
 
-
-
             var guiDisp = Application.Current.Dispatcher;
-            controller.Connection.ValueChanged += (s, e) => guiDisp.Invoke(() => { ConnectedBorder.Background = new SolidColorBrush(e.Value ? Colors.Green : Colors.DarkRed); });
 
+            Arduino.DataReceived += (s, e) => guiDisp.Invoke(() => 
+            {
+                ma1.Content = Arduino.ReadChar();
+
+            });
+
+            
             controller.LeftTrigger.ValueChanged += (s, e) => guiDisp.Invoke(() => 
             {
                 if (!invert && e.Value >= LeftTrigger.Value)
@@ -91,14 +98,6 @@ namespace EOD_WPF
             {
                 invert = e.Value;
             });
-
-
-            controller.LeftThumbclick.ValueChanged += (s, e) => guiDisp.Invoke(() => { LeftThumbCircle.Fill = new SolidColorBrush(e.Value ? Colors.LightGray : Colors.DarkGray); });
-            controller.RightThumbclick.ValueChanged += (s, e) => guiDisp.Invoke(() => { RightThumbCircle.Fill = new SolidColorBrush(e.Value ? Colors.LightGray : Colors.DarkGray); });
-
-            controller.LeftThumbstick.ValueChanged += (s, e) => guiDisp.Invoke(() => { LeftThumbPositionsCircle.Margin = new Thickness(45 * e.Value.X, -45 * e.Value.Y, 0.0, 0.0); });
-            controller.RightThumbstick.ValueChanged += (s, e) => guiDisp.Invoke(() => { RightThumbPositionsCircle.Margin = new Thickness(45 * e.Value.X, -45 * e.Value.Y, 0.0, 0.0); });
-
         }
 
         private void LeftShoulder_ValueChanged(object? sender, ValueChangeArgs<bool> e)
@@ -119,15 +118,8 @@ namespace EOD_WPF
                 byte[] par1 = new byte[] { motor };
                 byte[] msg = par1.Concat(BitConverter.GetBytes((char)speed)).ToArray();
                 Arduino.Write(msg, 0, 2);
-                //Log.Items.Add(new ListViewItem(new string[] { DateTime.Now.ToString("HH:mm:ss:ff"), $"Sended {speed} to motor {motor}" })).EnsureVisible();
             }
         }
-
-        private void log_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-
-        }
-
 
         private void comports_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -140,6 +132,7 @@ namespace EOD_WPF
             if (Arduino.IsOpen)
             {
                 Arduino.Close();
+                new LogItem($"Connection closed on {Arduino.PortName}").print(log);
             };
             try
             {
@@ -147,18 +140,15 @@ namespace EOD_WPF
                 Arduino.PortName = port;
                 Arduino.Open();
                 Arduino.ReceivedBytesThreshold = 20;
-                log.Items.Add(new string[] { "test", "test" });
+                new LogItem($"Connection established on {port}").print(log);
             }
             catch (Exception ex)
             {
+                new LogItem($"Can't connect to {port} stacktrace: {ex.StackTrace}").print(log);
             }
-
-
         }
 
    
-
-
         public class LogItem
         {
             public DateTime Timestamp { get; set; }
@@ -177,9 +167,14 @@ namespace EOD_WPF
             }
         }
 
-        private void LeftTrigger_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void LeftRumble_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
+            controller.LeftRumble.Rumble((float)RoughRumble.Value);
+        }
 
+        private void RightRumble_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            controller.RightRumble.Rumble((float)FineRumble.Value);
         }
     }
 }
